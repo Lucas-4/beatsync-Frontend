eede4e2f5ef2a067233fec265a92a218fd768f9c
unknown <diaslucas8822@gmail.com>
2024-01-12 00:13:05 -0300

    fized load more posts feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import { useEffect, useState } from "react";
 import Main from "../components/Main";
 
 function Home() {
-    const [posts, setPosts] = useState();
+    const [posts, setPosts] = useState([]);
     const [lastPostId, setLastPostId] = useState();
-    useEffect(() => {
+    const loadPosts = () => {
         fetch(
             process.env.REACT_APP_API_HOST + "/posts?lastPostId=" + lastPostId,
             {
@@ -22,40 +22,44 @@ function Home() {
                 return res.json();
             })
             .then((data) => {
-                setPosts(data.posts);
+                setPosts(posts.concat(data.posts));
                 setLastPostId(data.lastPostId);
             })
             .catch((error) => {
                 console.log(error);
             });
+    };
+    useEffect(() => {
+        loadPosts();
     }, []);
 
     return (
         <Main>
             <CreatePost />
-            {posts &&
-                posts.map((post) => {
-                    return (
-                        <Post
-                            key={post.post_id}
-                            username={post.username}
-                            display_name={post.display_name}
-                            content={post.content}
-                            likes={post.likes_num}
-                            is_liked={post.is_liked}
-                            post_id={post.post_id}
-                            comments_num={post.comments_num}
-                            song_id={post.song_id}
-                            playlist_id={post.playlist_id}
-                            profile_image_path={post.profile_image_path}
-                            post_image_path={post.post_image_path}
-                            belongs_to_current_user={
-                                post.belongs_to_current_user
-                            }
-                        />
-                    );
-                })}
-            <button>Load more posts...</button>
+            {posts.length === 0
+                ? null
+                : posts.map((post) => {
+                      return (
+                          <Post
+                              key={post.post_id}
+                              username={post.username}
+                              display_name={post.display_name}
+                              content={post.content}
+                              likes={post.likes_num}
+                              is_liked={post.is_liked}
+                              post_id={post.post_id}
+                              comments_num={post.comments_num}
+                              song_id={post.song_id}
+                              playlist_id={post.playlist_id}
+                              profile_image_path={post.profile_image_path}
+                              post_image_path={post.post_image_path}
+                              belongs_to_current_user={
+                                  post.belongs_to_current_user
+                              }
+                          />
+                      );
+                  })}
+            <button onClick={loadPosts}>Load more posts...</button>
         </Main>
     );
 }
